024756d145cbef7abeee3b8b631ec5425ab2817c
Se1getsu <kakky_parfait108@yahoo.co.jp>
2023-04-30 23:39:37 +0900

    First commit

---
+++ test01.rb
@@ -0,0 +1,15 @@
+# コメント
+puts "Hello, world"
+puts 'Hello, world'
+puts 1 + 2
+n = 1_980_000   #コメント
+puts "値段は#{n}円"
+puts `ruby --version`
+print <<-EOS1, <<-EOS2
+これはみかん
+間違いない
+EOS1
+価値は#{n}円
+EOS2
+a = true
+puts a.class
