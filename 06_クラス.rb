18f7ab949447c910738c7b4b2f4b17c1d60d0c2d
Se1getsu <kakky_parfait108@yahoo.co.jp>
2023-05-05 10:55:31 +0900

    improve: 冗長さを改善

--- 06_クラス.rb
+++ 06_クラス.rb
@@ -1,39 +1,39 @@
 
+# getter, setter を設定しないと、
+# デフォルトでは @name, @@count は外部から隠蔽される点に注意が必要
+
 class Human # < SuperClass
 
-    @@count = 0     # 外からはアクセス不可
+    @@count = 0     # クラス変数
 
-    # attr_reader :hoge  # これがないと h.hoge は外部から参照できない
-    # attr_writer :hoge  # これがないと h.hoge は外部から変更できない
-    attr_accessor :hoge  # 上記２つをまとめたものがこれ
+    # attr_reader :name  # これがないと h.name は外部から参照できない
+    # attr_writer :name  # これがないと h.name は外部から変更できない
+    attr_accessor :name  # 上記２つをまとめたものがこれ (複数は,区切り)
 
-    def setName(name)
-        @name = name
-    end
+    # 《 attr_accessor は以下の２つのメソッドの定義を自動化する》
+    #
+    # def name
+    #     @name     # return は省略可
+    # end
+    #
+    # def name=(new_name)
+    #     @name = new_name
+    # end
 
-    def getName
-        return @name
+    def self.count
+        @@count
     end
 
     # イニシャライザは省略可能
     def initialize(name=nil)
         @@count += 1
         @name = name if name
-        @hoge = "ほげぇ"
-    end
-
-    def self.getCount
-        @@count     # return は省略可
     end
 end
 
 sgt = Human.new
-shk = Human.new("Shimai ni Shake")
-sgt.setName "Seigetsu Masuta"
-p sgt.getName
-p shk.getName
-p Human.getCount
-
-p shk.hoge
-shk.hoge = "ほげほげぇ"
-p shk.hoge
+sgt.name = "霽月"
+shk = Human.new "終に鮭"
+p sgt.name
+p shk.name
+p Human.count
